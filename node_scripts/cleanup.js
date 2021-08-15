1218417caf1b172a69664fe4feca3e3eabfa322e
vincent <vincent.bulfon@gmail.com>
2021-08-15 18:43:29 +0200

    Add generated type cleanup script

---
+++ node_scripts/cleanup.js
@@ -0,0 +1,20 @@
+let fs = require('fs')
+
+function cleanupFiles({ path }) {
+  path.forEach((path) => {
+    //check if file exist or is a directory
+    if (fs.existsSync(path) && fs.lstatSync(path).isFile()) {
+      console.log(`Deleting file ${path}`);
+      fs.unlinkSync(path); //delet files
+    } else {
+      console.log(`File ${path} does not exist or is a directory`);
+    }
+  });
+  return;
+}
+
+console.log("Cleaning auto generated files...");
+
+cleanupFiles({ path: ["nexus-typegen.ts", "shema.graphql"] });
+
+console.log("Files sucessfully cleaned");
