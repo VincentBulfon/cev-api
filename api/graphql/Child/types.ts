63d148e04664f3d7947710f497fffce075a5f040
vincent <vincent.bulfon@gmail.com>
2021-08-16 18:01:56 +0200

    update child types & add tutor field

--- api/graphql/Child/types.ts
+++ api/graphql/Child/types.ts
@@ -3,9 +3,18 @@ import { objectType } from "nexus";
 export const child = objectType({
   name: "Child",
   definition(t) {
-    t.int("id");
-    t.string("name");
-    t.string("first_name");
-    t.date("birth_date");
+    t.nonNull.int("id");
+    t.nonNull.string("name");
+    t.nonNull.string("first_name");
+    t.nonNull.date("birth_date");
+    t.nonNull.int("tutor_id");
+    t.field("tutor", {
+      type: "User",
+      resolve: (root, _, ctx) => {
+        return ctx.prisma.users.findUnique({
+          where: { id: root.tutor_id },
+        });
+      },
+    });
   },
 });
