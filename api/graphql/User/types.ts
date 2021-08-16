24f03544775e05cd7c1cbaaa4eea452b85b5177f
vincent <vincent.bulfon@gmail.com>
2021-08-16 18:01:13 +0200

    Update user type & add child and orders relations

--- api/graphql/User/types.ts
+++ api/graphql/User/types.ts
@@ -1,17 +1,31 @@
-import { objectType } from "nexus";
+import { nonNull, objectType } from "nexus";
 
 export const user = objectType({
   name: "User",
   definition(t) {
-    t.int("id");
-    t.string("email");
-    t.string("name");
-    t.string("first_name");
-    t.string("password");
-    t.string("phone_number");
+    t.nonNull.int("id");
+    t.nonNull.string("name");
+    t.nonNull.string("first_name");
+    t.nonNull.string("password");
+    t.nonNull.string("phone_number");
     t.string("secondary_email");
-    t.date("created_at");
     t.date("vertified_at");
-    t.date("created_at");
+    t.nonNull.date("created_at");
+    t.list.field("children", {
+      type: "Child",
+      resolve: (root, _, ctx) => {
+        return ctx.prisma.children.findMany({
+          where: { tutor_id: root.id },
+        });
+      },
+    });
+    t.list.field("Orders", {
+      type: "Order",
+      resolve: (root, args, ctx) => {
+        return ctx.prisma.orders.findMany({
+          where: { users_id: root.id },
+        });
+      },
+    });
   },
 });
