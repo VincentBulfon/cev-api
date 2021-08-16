a5e367ccee0ad16396d988b48b936624f2439f13
vincent <vincent.bulfon@gmail.com>
2021-08-16 18:03:08 +0200

    update course types

--- api/graphql/Course/types.ts
+++ api/graphql/Course/types.ts
@@ -1,11 +1,40 @@
-import { objectType } from "nexus";
+import { arg, objectType } from "nexus";
+import { DateScalar } from "../scalars";
 
 export const course = objectType({
   name: "Course",
   definition(t) {
-    t.int("id"), t.int("day_of_week");
-    t.date("start_time");
-    t.date("end_time");
-    t.int("places");
+    t.nonNull.int("id"), t.int("day_of_week");
+    t.nonNull.date("start_time");
+    t.nonNull.date("end_time");
+    t.nonNull.int("places");
+    t.list.field("enfants", {
+      type: "Child",
+      resolve: (root, args, ctx) => {
+        return ctx.prisma.children.findMany({
+          where: { courses: { some: { id: { equals: root.id } } } },
+        });
+      },
+    });
+    t.list.field("cancellations", {
+      type: "Cancellation",
+      args: {
+        firstDate: arg({ type: "DateTime" }),
+        secondDate: arg({ type: "DateTime" }),
+      },
+      resolve: (root, args, ctx) => {
+        return ctx.prisma.cancellations.findMany({
+          where: {
+            AND: {
+              course_id: root.id,
+              date: {
+                gte: new Date(args.firstDate),
+                lt: new Date(args.secondDate),
+              },
+            },
+          },
+        });
+      },
+    });
   },
 });
