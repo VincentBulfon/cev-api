da6748800dee785359bb6b0da3f3598ed3c83471
vincent <vincent.bulfon@gmail.com>
2021-08-16 12:06:22 +0200

    add all oprations & types import on the server instance

--- api/graphql/index.ts
+++ api/graphql/index.ts
@@ -1,1 +1,7 @@
 export * from "./User";
+export * from "./Child";
+export * from "./Course";
+export * from "./Cancellations";
+export * from "./Options";
+export * from "./Prices";
+export * from "./Orders";
