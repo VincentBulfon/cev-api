1503606c0da3c8de556338edd5bacce4f2f4f24a
vincent <vincent.bulfon@gmail.com>
2021-08-16 17:50:06 +0200

    Update scalar definition

--- api/graphql/scalars.ts
+++ api/graphql/scalars.ts
@@ -1,3 +1,4 @@
+import { Kind } from "graphql";
 import { scalarType } from "nexus";
 
 export const DateScalar = scalarType({
@@ -6,5 +7,15 @@ export const DateScalar = scalarType({
   description: "Date custom scalar type",
   parseValue(value) {
     return new Date(value);
+  },
+  serialize(value) {
+    return value.getTime();
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return new Date(ast.value);
+    }
+
+    return null;
   },
 });
