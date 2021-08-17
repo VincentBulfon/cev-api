0098436cfddb51275d08ae00d89445a32e65fd7d
vincent <vincent.bulfon@gmail.com>
2021-08-17 11:10:49 +0200

    Add related field types

--- api/graphql/Options/types.ts
+++ api/graphql/Options/types.ts
@@ -1,9 +1,19 @@
 import { objectType } from "nexus";
+import { resolve } from "path/posix";
 
 export const option = objectType({
   name: "Option",
   definition(t) {
     t.nonNull.int("id"), t.nonNull.string("name");
     t.nonNull.date("created_at");
+    t.field("price", {
+      type: "Price",
+      resolve: (root, _, ctx) => {
+        return ctx.prisma.prices.findFirst({
+          orderBy: { created_at: "asc" },
+          take: 1,
+        });
+      },
+    });
   },
 });
--- api/graphql/Orders/types.ts
+++ api/graphql/Orders/types.ts
@@ -6,5 +6,22 @@ export const order = objectType({
     t.nonNull.int("id");
     t.nonNull.date("created_at");
     t.date("cancelled_at");
+    t.nonNull.int("user_id");
+    t.list.field("option_set", {
+      type: "OptionSet",
+      resolve: (root, _, ctx) => {
+        return ctx.prisma.options_set.findMany({
+          where: { order_id: { equals: root.id } },
+        });
+      },
+    });
+    t.field("user", {
+      type: "User",
+      resolve: (root, _, ctx) => {
+        return ctx.prisma.users.findFirst({
+          where: { id: { equals: root.user_id } },
+        });
+      },
+    });
   },
 });
