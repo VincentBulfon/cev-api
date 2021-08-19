d04528262ff0c948d83cf4b3807bbb86206ed512
vincent <vincent.bulfon@gmail.com>
2021-08-19 08:50:38 +0200

    Update server starting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { server } from "./server";
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`
+    🚀  Server is ready at ${url}
+    📭  Query at https://studio.apollographql.com/dev
+  `);
 });
