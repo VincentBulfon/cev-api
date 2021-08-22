801385c1c283ae6da5381e0f729f710ad918b635
vincent <vincent.bulfon@gmail.com>
2021-08-22 20:35:42 +0200

    Update schield

--- src/shield/middlewares.ts
+++ src/shield/middlewares.ts
@@ -5,11 +5,6 @@ export const middlewares = shield({
   Query: {
     users: rules.isAuthenticatedUser,
     children: rules.isAuthenticatedUser,
-    courses: allow,
-    cancellations: allow,
-    options: allow,
-    prices: allow,
-    currentUser: allow,
   },
   Mutation: {
     deleteOneUsers: rules.isAuthenticatedUser,
